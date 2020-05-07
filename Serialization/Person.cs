5eeefe4053e54029918da0051e190885ed4a5b21
MatiasRojasG <62114178+MatiasRojasG@users.noreply.github.com>
2020-05-07 09:46:37 -0400

    Ejercicio terminado sin lista

--- Serialization/Person.cs
+++ Serialization/Person.cs
@@ -1,10 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Runtime.Serialization;
+
+
+
 namespace Serialization
 {
+    [Serializable]
     public class Person
     {
-        public Person()
+        string name;
+        string lastname;
+        int age;
+
+        public Person(string Name, string LastName, int Age)
         {
+            this.name = Name;
+            this.lastname = LastName;
+            this.age = Age;
+        }
+        public string Info()
+        {
+            return "Nombre: " + name + " Apellido: " + lastname + " Edad: " + age;
         }
     }
 }
--- Serialization/Program.cs
+++ Serialization/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
 
 namespace Serialization
 {
@@ -6,7 +11,40 @@ namespace Serialization
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string respuesta;
+            string nombre;
+            string apellido;
+            int edad;
+            Console.WriteLine("Seleccione una opción:");
+            Console.WriteLine("1) Crear y serializar persona");
+            Console.WriteLine("2) Leer Persona");
+            respuesta= Console.ReadLine();
+            if (respuesta=="1")
+            {
+
+                Console.WriteLine("Ingrese el nombre");
+                nombre = Console.ReadLine();
+                Console.WriteLine("Ingrese el apellido");
+                apellido = Console.ReadLine();
+                Console.WriteLine("Ingrese la edad");
+                edad = Convert.ToInt32(Console.ReadLine());
+                Person persona = new Person(nombre, apellido, edad);
+
+                BinaryFormatter formatter = new BinaryFormatter();
+                Stream myStream = new FileStream("Personas", FileMode.Create, FileAccess.Write, FileShare.None);
+                formatter.Serialize(myStream, persona);
+                myStream.Close();
+            }
+            if (respuesta=="2")
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                Stream myStream = new FileStream("Personas", FileMode.Open, FileAccess.Read, FileShare.None);
+                Person persona = (Person)formatter.Deserialize(myStream);
+                myStream.Close();
+                Console.WriteLine(persona.Info());
+
+
+            }
         }
     }
 }
