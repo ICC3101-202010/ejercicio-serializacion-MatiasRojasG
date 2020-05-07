c2f610d42a76211f0c0b1664f6b48cea1eb11df8
MatiasRojasG <62114178+MatiasRojasG@users.noreply.github.com>
2020-05-07 09:19:33 -0400

    first commit

---
+++ Serialization/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Serialization
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
